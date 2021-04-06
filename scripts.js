7789a426f4df53085ff9e207dea294f1ab21090c
vmanz23 <vintagemodern@zoho.com>
2021-04-06 16:57:56 -0700

    jQuery scripts update

--- scripts.js
+++ scripts.js
@@ -1,6 +1,8 @@
+//Adding a new item to the list of items:
+//jQuery
+
 function newItem() {
 
-//Adding a new item to the list of items:
   let li = $('<li></li>');
   let inputValue = $('#input').val();
   li.append(document.createTextNode(inputValue));
