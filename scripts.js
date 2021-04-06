8222ce9d60b4fb651813c927ee0db490c1a17e42
vmanz23 <vintagemodern@zoho.com>
2021-04-06 14:08:51 -0700

    Initial commit

---
+++ scripts.js
@@ -0,0 +1,32 @@
+function newItem() {
+
+//Adding a new item to the list of items:
+  let li = $('<li></li>');
+  let inputValue = $('#input').val();
+  li.append(document.createTextNode(inputValue));
+
+  if (inputValue === '') {
+    alert('You must write something')
+  } else {
+    let list = $('#list');
+    list.append(li);
+  }
+
+//Crossing out an item from the list of items:
+  li.on('dblclick', function(){
+  li.toggleClass('strike');
+  });
+
+// Adding the delete button "X":
+let crossOutButton = $('<crossOutButton></crossOutButton>');
+crossOutButton.append(document.createTextNode('X'));
+li.append(crossOutButton);
+
+crossOutButton.on('click', function(){
+  li.addClass('delete');
+});
+
+//Reordering the items:
+  $('#list').sortable();
+
+}
